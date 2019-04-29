7e062aa223c4d3ce7cd786beafb1a5d5f5bc7314
SPortugall <s.portugall@hm.edu>
2019-04-29 21:33:58 +0200

    rating change

--- src/main/java/base/appstore/RatingRepository.java
+++ src/main/java/base/appstore/RatingRepository.java
@@ -2,12 +2,13 @@ package base.appstore;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface RatingRepository extends JpaRepository<Rating, Long> {
 
-    @Query("SELECT appId, userId FROM Rating WHERE appId = ?1 and userId = ?2")
-    List<Rating> findByAppAndUserId(Long appId, Long userId);
+    @Query("SELECT appId, userId FROM Rating WHERE appId = :appId and userId = :userId")
+    List<Rating> findByAppAndUserId(@Param("appId") Long appId, @Param("userId") Long userId);
 
 }
