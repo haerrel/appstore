599be7ddcda8260881bb012f343df849b16d412f
SPortugall <s.portugall@hm.edu>
2019-04-29 21:30:43 +0200

    rating change

--- src/main/java/base/appstore/AppController.java
+++ src/main/java/base/appstore/AppController.java
@@ -1,6 +1,7 @@
 package base.appstore;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
@@ -16,76 +17,85 @@ import java.util.stream.Stream;
 @CrossOrigin
 public class AppController {
 
-  @Autowired
-  private AppRepository appRepository;
+    @Autowired
+    private AppRepository appRepository;
+
+    @Autowired
+    private RatingRepository ratingRepository;
 
 
-  @GetMapping
-  public List<App> all(@RequestParam(required = false) String search,
-                       @RequestParam(required = false) List<String> tag,
-                       @RequestParam(required = false) Integer limit,
-                       @RequestParam(required = false) String filter
-  ) {
-    if (Objects.isNull(search) && Objects.isNull(filter) && Objects.isNull(tag) && Objects.isNull(tag)) {
-      return appRepository.findAll();
+    @GetMapping
+    public List<App> all(@RequestParam(required = false) String search,
+                         @RequestParam(required = false) List<String> tag,
+                         @RequestParam(required = false) Integer limit,
+                         @RequestParam(required = false) String filter
+    ) {
+        if (Objects.isNull(search) && Objects.isNull(filter) && Objects.isNull(tag) && Objects.isNull(tag)) {
+            return appRepository.findAll();
+        }
+
+        Stream<App> appStream = appRepository.findAll().stream();
+        if (!Objects.isNull(search)) {
+            appStream = appStream.filter(app -> app.getTitle().startsWith(search));
+        }
+        if (!Objects.isNull(tag)) {
+            appStream = appStream.filter(app -> {
+                List<String> tags = Arrays.asList(app.getTags().split(","));
+                tags.retainAll(tag); //TODO throws exception
+                return !tags.isEmpty();
+            });
+        }
+        if (!Objects.isNull(filter)) {
+            switch (filter.toLowerCase()) {
+                case "cheapest":
+                    appStream = appStream.sorted(Comparator.comparing(App::getPrice));
+                    break;
+                case "newest":
+                    appStream = appStream.sorted(Comparator.comparing(App::getDatePublished));
+                    break;
+                case "famous":
+                    appStream = appStream.sorted(Comparator.comparing(App::getRating));
+                    break;
+            }
+        }
+        if (!Objects.isNull(limit)) {
+            appStream = appStream.limit(limit);
+        }
+        return appStream.collect(Collectors.toList());
     }
 
-    Stream<App> appStream = appRepository.findAll().stream();
-    if (!Objects.isNull(search)) {
-      appStream = appStream.filter(app -> app.getTitle().startsWith(search));
+    @GetMapping("{id}")
+    public App find(@PathVariable Long id) {
+        return appRepository.findById(id).orElseThrow(() -> new AppNotFoundException(id));
     }
-    if (!Objects.isNull(tag)) {
-      appStream = appStream.filter(app -> {
-        List<String> tags = Arrays.asList(app.getTags().split(","));
-        tags.retainAll(tag); //TODO throws exception
-        return !tags.isEmpty();
-      });
+
+    @PostMapping
+    public App create(@RequestBody App input) {
+        return appRepository.save(new App(input.getText(), input.getTags(), input.getTitle(), input.getPrice(), input.getDatePublished()));
     }
-    if (!Objects.isNull(filter)) {
-      switch (filter.toLowerCase()) {
-        case "cheapest":
-          appStream = appStream.sorted(Comparator.comparing(App::getPrice));
-          break;
-        case "newest":
-          appStream = appStream.sorted(Comparator.comparing(App::getDatePublished));
-          break;
-        case "famous":
-          appStream = appStream.sorted(Comparator.comparing(App::getRating));
-          break;
-      }
+
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable Long id) {
+        appRepository.deleteById(id);
     }
-    if (!Objects.isNull(limit)) {
-      appStream = appStream.limit(limit);
+
+    @PutMapping("{id}")
+    public App update(@PathVariable Long id, @RequestBody App input) {
+        return appRepository.findById(id).map(app -> {
+            app.setId(id);
+            app.setTitle(input.getTitle());
+            app.setText(input.getText());
+            app.setTags(input.getTags());
+            return appRepository.save(app);
+        }).orElseGet(() -> {
+            input.setId(id);
+            return appRepository.save(input);
+        });
     }
-    return appStream.collect(Collectors.toList());
-  }
 
-  @GetMapping("{id}")
-  public App find(@PathVariable Long id) {
-    return appRepository.findById(id).orElseThrow(() -> new AppNotFoundException(id));
-  }
+    @GetMapping("/{id}/rating/{userID}")
+    public List<Rating> find(@PathVariable Long id, @PathVariable Long userID) {
+        return ratingRepository.findByAppAndUserId(id, userID);
+    }
 
-  @PostMapping
-  public App create(@RequestBody App input) {
-    return appRepository.save(new App(input.getText(), input.getTags(), input.getTitle(), input.getPrice(), input.getDatePublished()));
-  }
-
-  @DeleteMapping("{id}")
-  public void delete(@PathVariable Long id) {
-    appRepository.deleteById(id);
-  }
-
-  @PutMapping("{id}")
-  public App update(@PathVariable Long id, @RequestBody App input) {
-    return appRepository.findById(id).map(app -> {
-      app.setId(id);
-      app.setTitle(input.getTitle());
-      app.setText(input.getText());
-      app.setTags(input.getTags());
-      return appRepository.save(app);
-    }).orElseGet(() -> {
-      input.setId(id);
-      return appRepository.save(input);
-    });
-  }
 }
--- src/main/java/base/appstore/RatingRepository.java
+++ src/main/java/base/appstore/RatingRepository.java
@@ -1,6 +1,13 @@
 package base.appstore;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface RatingRepository extends JpaRepository<App, Long> {
+import java.util.List;
+
+public interface RatingRepository extends JpaRepository<Rating, Long> {
+
+    @Query("SELECT appId, userId FROM Rating u WHERE appId = ?1 and userId = ?2")
+    List<Rating> findByAppAndUserId(Long appId, Long userId);
+
 }
