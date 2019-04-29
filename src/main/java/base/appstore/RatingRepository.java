049fb8b27edde473a54d11b7a02dcc352d127c09
SPortugall <s.portugall@hm.edu>
2019-04-29 21:32:29 +0200

    rating

--- src/main/java/base/appstore/RatingRepository.java
+++ src/main/java/base/appstore/RatingRepository.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public interface RatingRepository extends JpaRepository<Rating, Long> {
 
-    @Query("SELECT appId, userId FROM Rating u WHERE appId = ?1 and userId = ?2")
+    @Query("SELECT appId, userId FROM Rating WHERE appId = ?1 and userId = ?2")
     List<Rating> findByAppAndUserId(Long appId, Long userId);
 
 }
