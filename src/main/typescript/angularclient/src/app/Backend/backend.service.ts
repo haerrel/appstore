558deebee434bb4df3d8b4809453ce4b3374a15f
SPortugall <s.portugall@hm.edu>
2019-05-11 20:53:36 +0200

    add postinstall for prod mode in angular

--- src/main/typescript/angularclient/src/app/Backend/backend.service.ts
+++ src/main/typescript/angularclient/src/app/Backend/backend.service.ts
@@ -3,7 +3,7 @@ import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {map} from 'rxjs/operators';
 import {App} from '../shared/app';
-import {environment} from "../environment/environment";
+import {environment} from "../environment/environment.prod";
 
 @Injectable({
   providedIn: 'root'
