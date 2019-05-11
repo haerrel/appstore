0a2a7bd9a16e5561ddaa7d2663225debb2b903a8
SPortugall <s.portugall@hm.edu>
2019-05-11 20:59:25 +0200

    add postinstall for prod mode in angular

--- src/main/typescript/angularclient/src/app/Backend/backend.service.ts
+++ src/main/typescript/angularclient/src/app/Backend/backend.service.ts
@@ -10,7 +10,7 @@ import {environment} from "../environment/environment";
 })
 export class BackendService {
 
-  private endpoint = environment.baseUrl;
+  private endpoint = 'https://app-hm.herokuapp.com/';
 
   private httpOptions = {
     headers: new HttpHeaders({
