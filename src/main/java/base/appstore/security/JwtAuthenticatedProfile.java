4f5691a4039a76656fac5b0557f8c952bfe81865
Gudrun Socher <gudrun.socher@hm.edu>
2019-05-16 00:17:13 +0200

    unused imports

--- src/main/java/base/appstore/security/JwtAuthenticatedProfile.java
+++ src/main/java/base/appstore/security/JwtAuthenticatedProfile.java
@@ -3,7 +3,6 @@ package base.appstore.security;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 
-import java.util.ArrayList;
 import java.util.Collection;
 
 @SuppressWarnings("serial")
--- src/main/java/base/appstore/security/JwtAuthenticationProvider.java
+++ src/main/java/base/appstore/security/JwtAuthenticationProvider.java
@@ -7,7 +7,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
 
