e59ad61a645af273b4ff3d22e9923aa2554a1769
Andreas Hartung <ahartung@outlook.com>
2019-05-25 21:21:58 +0200

    frontend: route to login page on logout

--- src/main/typescript/angularclient/src/app/header/header.component.ts
+++ src/main/typescript/angularclient/src/app/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {AuthService} from '../services/auth/auth.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -8,13 +9,14 @@ import {AuthService} from '../services/auth/auth.service';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit() {
   }
 
   logout() {
     this.authService.logout();
+    this.router.navigateByUrl('/login');
   }
 
   isLoggedIn(): boolean {
